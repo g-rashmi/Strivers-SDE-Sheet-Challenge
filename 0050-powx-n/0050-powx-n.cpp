6c66c6402bd45d8542bf0ca96f2a467e5ce87229
Rashmi Gupta <110086481+g-rashmi@users.noreply.github.com>
2023-06-10 22:45:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double y=pow(x,n);
+         double y=1.0;
+        long long nn=n;
+        if(nn<0)
+            nn=-1*nn;
+        while(nn){
+            if(nn%2){
+                y=y*x;
+            nn=nn-1;
+        } 
+            else{
+                x=x*x;
+                nn=nn/2;
+            }
+        } 
+        if(n<0)
+            y=1.0/y;
         return y;
     }
 };
